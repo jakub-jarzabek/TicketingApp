4115ac673b1d39c6e998e661aace7878205d5cb9
Kacper <kacper.reja@gmail.com>
2021-08-08 00:24:24 +0200

    test

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser = require('body-parser')
 const port = 3000
 const authRoutes = require('./routes/index.js')
+const mongoose = require('mongoose')
+
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
@@ -12,12 +14,20 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(express.json())
 
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 app.use(authRoutes)
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
+const dbURI = 'mongodb+srv://todoApp:jms123@clustertodo.i83dl.mongodb.net/ticketing-app'
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+
+
+
+
+
+
