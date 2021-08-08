ae3bec0df802941002e0e8c8984b3b60483c7db7
Kacper <kacper.reja@gmail.com>
2021-08-08 04:29:11 +0200

    ticketSchema

---
+++ server/models/Ticket.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const ticketSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    issuedBy: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    isResolved: {
+        type: Boolean,
+        required: true
+    },
+    resolveDate: {
+        type: Date,
+        required: false
+    },
+
+    resolvedBy: {
+        type: String,
+        required: false
+    },
+
+    comment: {
+        type: String,
+        required: false
+    }
+})
+
+
+
+const Ticket = mongoose.model('ticket', ticketSchema)
+
+module.exports = Ticket;
